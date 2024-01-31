9d98d7f2e6db622bb2335faabf5c57471bac2d93
Gustavo Mayorga <redvioletoficial@gmail.com>
2024-01-31 12:57:27 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Controler_Alert_System';
+}
