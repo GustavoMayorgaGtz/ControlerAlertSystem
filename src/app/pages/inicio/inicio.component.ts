7b690b64e0973d393f17d3d3756b9bb5a68c393d
Gustavo Mayorga <redvioletoficial@gmail.com>
2024-02-08 11:22:06 -0600

    save

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class InicioComponent {
 
+
+
+
+  facebook_event(){
+    window.location.href = "https://www.facebook.com/checkpoint/1501092823525282/?next=https%3A%2F%2Fwww.facebook.com%2Fcontroler.com.mx";
+  }
+  instagram_event(){
+    window.location.href = "https://www.instagram.com/controler.mx/";
+  }
+
+  schneider_electric_event(){
+    window.location.href = "https://www.se.com/mx/es/";
+  }
+  inductive_automation_event(){
+    window.location.href = "https://inductiveautomation.com/";
+  }
+
+  store_event(){
+    window.location.href = "https://controler.mx/";
+    
+  }
+
 }
